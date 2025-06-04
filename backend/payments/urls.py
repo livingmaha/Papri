66023f95f8f6b8193de4c91feb1975dbf395dbb6
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-06-04 03:00:50 -0700

    Update urls.py

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -1,1 +1,17 @@
+# backend/payments/urls.py
+from django.urls import path
+from .views import (
+    InitializePaystackPaymentView, 
+    PaystackCallbackView,
+    ListPlansView
+)
 
+app_name = 'payments'  # Namespace for this app's URLs
+
+urlpatterns = [
+    path('plans/', ListPlansView.as_view(), name='list_plans'),
+    path('paystack/initialize/', InitializePaystackPaymentView.as_view(), name='paystack_initialize'),
+    
+    # This URL name ('paystack_callback') MUST match settings.PAYSTACK_CALLBACK_URL_NAME
+    path('paystack/callback/', PaystackCallbackView.as_view(), name='paystack_callback'), 
+]
