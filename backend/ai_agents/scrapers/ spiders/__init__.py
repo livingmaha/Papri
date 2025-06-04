c9a2d5c3bc7bfc1794495d96db0439021b78ac5e
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-06-04 03:10:28 -0700

    Create __init__.py

---
+++ backend/ai_agents/scrapers/ spiders/__init__.py
@@ -0,0 +1,7 @@
+# backend/ai_agents/scrapers/spiders/__init__.py
+
+# This file makes 'spiders' a Python sub-package of 'scrapers'.
+# This is where all your Scrapy spiders will reside.
+# You might import all spiders here to make them discoverable if needed, e.g.:
+# from .peertube_spider import PeertubeSpider
+# from .another_platform_spider import AnotherPlatformSpider
